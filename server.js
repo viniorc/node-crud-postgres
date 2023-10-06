3dd2ffbdfc8d5d530bc4e316393731d000171714
VINICIUS OLIVEIRA ROMANO CONCEIÇAO <VINICIUS4181@DIGITALCOLLEGE.LOCAL>
2023-10-05 23:07:47 -0300

    testando servidor e o get produtos

---
+++ server.js
@@ -0,0 +1,24 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+const host = 'localhost' //127.0.0.1
+const port = 5000
+
+// Declare a route
+fastify.get('/', function handler (request, reply) {
+  return { hello: 'Servidor Rodando' }
+})
+
+fastify.get('/produtos', function handler (request, reply) {
+  return { hello: 'Produtos Rodando' }
+})
+
+// Run the server!
+fastify.listen({
+  port,
+  host
+}).then(() => console.log(`Servidor rodando em http://${host}:${port}`))
+.catch(err => console.log(`Erro ao subir servidor ${err}`))
